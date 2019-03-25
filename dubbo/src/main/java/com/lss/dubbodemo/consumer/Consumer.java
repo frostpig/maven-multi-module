bef6252b78e087d3916953a99f7e43b11acea2d7
frostpig <1397233369@qq.com>
2019-03-25 19:20:36 +0800

    初学dubbo

---
+++ dubbo/src/main/java/com/lss/dubbodemo/consumer/Consumer.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2018 jumei, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.lss.dubbodemo.consumer;
+
+import com.lss.dubbodemo.service.DemoService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Function: Please Descrip This Class.
+ * <p>
+ * Created by shuangshuangl on 2019/3/14.
+ * Copyright (c) 2018,shuangshuangl@jumei.com All Rights Reserved.
+ */
+public class Consumer {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"spring/dubbo-demo-consumer.xml"});
+        DemoService demoService = (DemoService) context.getBean("demoService");
+        String hello = demoService.hello("World");
+        System.out.println("Hello");
+    }
+}
